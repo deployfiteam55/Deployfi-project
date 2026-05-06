b34cf6e5a4a242f0f3815be94b075efa593245cc
chirayujain362005@gmail.com <chirayujain362005@gmail.com>
2026-05-06 09:38:15 +0530

    initial setup

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,25 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { WalletProvider } from '@solana/wallet-adapter-react'
+import { WalletModalProvider } from '@solana/wallet-adapter-react-ui'
+import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom'
+import { ConnectionProvider } from '@solana/wallet-adapter-react'
+import '@solana/wallet-adapter-react-ui/styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
+)
+const endpoint = 'https://api.devnet.solana.com'
+const wallets = [new PhantomWalletAdapter()]
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ConnectionProvider endpoint={endpoint}>
+    <WalletProvider wallets={wallets} autoConnect>
+      <WalletModalProvider>
+        <App />
+      </WalletModalProvider>
+    </WalletProvider>
+  </ConnectionProvider>
 )
